66071eade3f2033077c9d0d09224fc5ce5b0c3c0
Andreas Gabor <andreas@rob.ag>
2020-07-27 09:59:07 +0200

    init keymap

---
+++ keyboards/kyria/keymaps/an-tex/config.h
@@ -0,0 +1,13 @@
+#ifdef RGBLIGHT_ENABLE
+  #define RGBLIGHT_ANIMATIONS
+  #define RGBLIGHT_HUE_STEP 8
+  #define RGBLIGHT_SAT_STEP 8
+  #define RGBLIGHT_VAL_STEP 8
+  #define RGBLIGHT_SLEEP
+#endif
+
+#define ENCODER_RESOLUTION 2
+
+#define MASTER_LEFT
+
+#define TAP_CODE_DELAY 10
